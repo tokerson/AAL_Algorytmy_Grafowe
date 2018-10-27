ce603f3539f43b47ee9cc2a36f2df7128b386708
tokerson <kuba.tokarzewski@gmail.com>
2018-10-28 01:25:29 +0200

    Added readme.txt describing running process and added two new, faster DFS algorythms. Better recursive one and non-recursive one.

--- AAL/src/Generator/GraphGenerator.java
+++ AAL/src/Generator/GraphGenerator.java
@@ -1,7 +1,5 @@
 package Generator;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.util.Random;
 
 public class GraphGenerator {
@@ -12,21 +10,15 @@ public class GraphGenerator {
 
             int size = Integer.parseInt(args[0]);
 
-            try {
-                PrintWriter printWriter = new PrintWriter("in.txt");
                 Random rand = new Random();
 
-                printWriter.println(size);
+                System.out.println(size);
 
                 for (int i = 0; i < size; ++i) {
                     int neighbor = rand.nextInt(size) + 1;
-                    printWriter.format("%d %d\n",i+1,neighbor);
+                    System.out.format("%d %d\n",i+1,neighbor);
                 }
 
-                printWriter.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
--- AAL/src/Main.java
+++ AAL/src/Main.java
@@ -1,49 +1,74 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Stack;
+import java.util.concurrent.Callable;
 
 public class Main {
 
     public static void main(String[] args){
 
-//        Graph graph = new Graph(5);
-//
-//        graph.addEdge(1,4);
-//        graph.addEdge(4,3);
-//        graph.addEdge(3,1);
-//        graph.addEdge(2,3);
-//        graph.addEdge(5,2);
+        Graph graph = createGraph();
 
-//        Graph graph = new Graph(6);
-//
-//        graph.addEdge(1,3);
-//        graph.addEdge(3,5);
-//        graph.addEdge(4,2);
-//        graph.addEdge(2,6);
-
-//        Graph graph = new Graph(4);
-//
-//        graph.addEdge(1,2);
-//        graph.addEdge(2,1);
-//        graph.addEdge(3,2);
-//        graph.addEdge(4,2);
-//
 //        graph.printEdges();
-//
-//        System.out.println(howManyScouts(graph));
+        int scouts;
+        long startTime = System.nanoTime();
+        scouts = howManyScouts(graph);
+        long endTime = System.nanoTime();
+        System.out.println(scouts);
+        System.out.println("Your time is " + (endTime - startTime) + " ns");
 
+        startTime = System.nanoTime();
+        scouts = howManyScouts2(graph);
+        endTime = System.nanoTime();
+        System.out.println(scouts);
+        System.out.println("Your time is " + (endTime - startTime) + " ns");
 
-
-        Graph graph = createGraphFromFile("in.txt");
-        graph.printEdges();
-        System.out.println(howManyScouts(graph));
+        startTime = System.nanoTime();
+        scouts = howManyScouts3(graph);
+        endTime = System.nanoTime();
+        System.out.println(scouts);
+        System.out.println("Your time is " + (endTime - startTime) + " ns");
     }
 
-    public static void DFS(Graph graph,boolean[] visited,int v) {
+    public static void DFS_Recursive(Graph graph, boolean[] visited, int v) {
         visited[v] = true;
         for( int i = 0; i < graph.getSize() ; ++i) {
             if( graph.areNodesConnected(v + 1, i + 1) && visited[i] == false ) {
-                DFS(graph,visited,i);
+                DFS_Recursive(graph,visited,i);
+            }
+        }
+    }
+
+    public static void DFS_Recursive2(Graph graph, boolean[] visited, int v) {
+        visited[v] = true;
+        Integer[] neighbors = graph.getNode(v + 1).getNeighbors();
+        for( int i = 0; i < neighbors.length ; ++i) {
+            if( visited[neighbors[i] - 1] == false ) {
+                DFS_Recursive2(graph,visited,neighbors[i] - 1);
+            }
+        }
+    }
+
+
+    public static void DFS_Stack(Graph graph,boolean[] visited,int node) {
+        int size = graph.getSize();
+
+        Stack stack = new Stack();
+
+        stack.push(node);
+        visited[node] = true;
+
+        while(!stack.empty()) {
+            node = (int) stack.pop();
+
+            Integer[] neighbors = graph.getNode(node + 1).getNeighbors();
+
+            for( int i = 0; i < neighbors.length ; ++i) {
+                if( visited[neighbors[i] - 1] == false ) {
+                    visited[neighbors[i] - 1] = true;
+                    stack.push(neighbors[i] - 1);
+                }
             }
         }
     }
@@ -56,26 +81,51 @@ public class Main {
         for(int i = 0 ; i < size ; ++i) {
             if(visited[i] == false) {
                 ++scouts;
-                DFS(graph,visited,i);
+                DFS_Recursive(graph,visited,i);
             }
         }
         return scouts;
     }
 
-    public static Graph createGraphFromFile(String filename) {
 
-        File file = new File(filename);
+    public static int howManyScouts2(Graph graph) {
+        int scouts = 0;
+        int size = graph.getSize();
+        boolean[] visited = new boolean[size];
+
+        for(int i = 0 ; i < size ; ++i) {
+            if(visited[i] == false) {
+                ++scouts;
+                DFS_Recursive2(graph,visited,i);
+            }
+        }
+        return scouts;
+    }
+
+    public static int howManyScouts3(Graph graph) {
+        int scouts = 0;
+        int size = graph.getSize();
+        boolean[] visited = new boolean[size];
+
+        for(int i = 0 ; i < size ; ++i) {
+            if(visited[i] == false) {
+                ++scouts;
+                DFS_Stack(graph,visited,i);
+            }
+        }
+        return scouts;
+    }
+
+    public static Graph createGraph() {
 
         try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
             String str = bufferedReader.readLine();
-//            System.out.println(str);
 
             Graph graph = new Graph(Integer.parseInt(str));
 
             while((str = bufferedReader.readLine()) != null) {
-//                System.out.println(str);
                 String[] numbers = (str.split("\\s+"));
                 graph.addEdge(Integer.parseInt(numbers[0]),Integer.parseInt(numbers[1]));
             }
@@ -83,12 +133,9 @@ public class Main {
             bufferedReader.close();
             return graph;
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
 
         return null;
 
